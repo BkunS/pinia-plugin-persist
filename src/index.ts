e1f60ef24c7fbae57c3ddcdd86aa20466634d894
BkunS <bkuns23@outlook.com>
2022-04-27 09:30:13 +0800

    fix bugs, update dependencies to support only vue 3

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,72 @@
-import { PiniaPluginContext } from 'pinia'
+import { PiniaPluginContext } from 'pinia';
 
 export interface PersistStrategy {
-  key?: string;
-  storage?: Storage;
-  paths?: string[];
+	key?: string;
+	storage?: Storage;
+	paths?: string[];
 }
 
 export interface PersistOptions {
-  enabled: true;
-  strategies?: PersistStrategy[];
+	enabled: true;
+	strategies?: PersistStrategy[];
 }
 
 type Store = PiniaPluginContext['store'];
 type PartialState = Partial<Store['$state']>;
 
 declare module 'pinia' {
-  export interface DefineStoreOptionsBase<S, Store> {
-    persist?: PersistOptions;
-  }
+	export interface DefineStoreOptionsBase<S, Store> {
+		persist?: PersistOptions;
+	}
 }
+
+const defaultStorage = localStorage;
 
 export const updateStorage = (strategy: PersistStrategy, store: Store) => {
-  const storage = strategy.storage || sessionStorage
-  const storeKey = strategy.key || store.$id
+	const storage = strategy.storage || defaultStorage;
+	const storeKey = strategy.key || store.$id;
 
-  if (strategy.paths) {
-    const partialState = strategy.paths.reduce((finalObj, key) => {
-      finalObj[key] = store.$state[key]
-      return finalObj
-    }, {} as PartialState)
+	if (strategy.paths) {
+		const partialState = strategy.paths.reduce((finalObj, key) => {
+			finalObj[key] = store.$state[key];
+			return finalObj;
+		}, {} as PartialState);
 
-    storage.setItem(storeKey, JSON.stringify(partialState))
-  } else {
-    storage.setItem(storeKey, JSON.stringify(store.$state))
-  }
-}
+		storage.setItem(storeKey, JSON.stringify(partialState));
+	} else {
+		storage.setItem(storeKey, JSON.stringify(store.$state));
+	}
+};
 
 export default ({ options, store }: PiniaPluginContext): void => {
-  if (options.persist?.enabled) {
-    const defaultStrat: PersistStrategy[] = [{
-      key: store.$id,
-      storage: sessionStorage,
-    }]
+	if (options.persist?.enabled) {
+		const defaultStrat: PersistStrategy[] = [
+			{
+				key: store.$id,
+				storage: defaultStorage,
+			},
+		];
 
-    const strategies = options.persist?.strategies?.length ? options.persist?.strategies : defaultStrat
+		const strategies: PersistStrategy[] = options.persist?.strategies?.length
+			? options.persist?.strategies
+			: defaultStrat;
 
-    strategies.forEach((strategy) => {
-      const storage = strategy.storage || sessionStorage
-      const storeKey = strategy.key || store.$id
-      const storageResult = storage.getItem(storeKey)
+		strategies.forEach(strategy => {
+			const storage = strategy.storage || defaultStorage;
+			const storeKey = strategy.key || store.$id;
+			const storageResult = storage.getItem(storeKey);
 
-      if (storageResult) {
-        store.$patch(JSON.parse(storageResult))
-        updateStorage(strategy, store)
-      }
-    })
+			if (storageResult) {
+				store.$patch(JSON.parse(storageResult));
+			} else {
+				updateStorage(strategy, store);
+			}
+		});
 
-    store.$subscribe(() => {
-      strategies.forEach((strategy) => {
-        updateStorage(strategy, store)
-      })
-    })
-  }
-}
+		store.$subscribe(() => {
+			strategies.forEach(strategy => {
+				updateStorage(strategy, store);
+			});
+		});
+	}
+};
